e47c07cbd64521c01496d1f1ad621cc7d86575f6
cocola4 <wastf41dshak@gmail.com>
2023-08-24 05:03:02 +0000

    [Finish]exercise3

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,61 @@
+class BooksController < ApplicationController
+  
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @new_book = @book  # エラーメッセージを表示するために @new_book に設定
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+  
+  
+  def index
+    @books = Book.all
+    @user = current_user
+    @new_book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @user = current_user
+    @new_book = Book.new
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  def edit
+    book = Book.find(params[:id])
+    unless book.user.id == current_user.id
+      redirect_to books_path
+    end
+    
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(book.id)
+    else
+      @book = book
+      render :edit
+    end
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+end
