a8c803cede72d09e91afabcfa4617d6695f56a3c
cocola4 <wastf41dshak@gmail.com>
2023-09-01 11:13:17 +0000

    changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_20_052941) do
+ActiveRecord::Schema.define(version: 2023_09_01_111120) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
